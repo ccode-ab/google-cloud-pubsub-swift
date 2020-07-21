dabc424d20f0e938edd96bcfae3e95900d293679
Gustaf Rosenblad <gustaf@ccode.se>
2020-07-21 07:08:00 +0200

    feat: Added support for subscriptions

--- Sources/GoogleCloudPubSub/Message.swift
+++ Sources/GoogleCloudPubSub/Message.swift
@@ -8,6 +8,13 @@ public struct Message {
     public var data: Data
     public var attributes: [String: String]
 
+    init(id: String, published: Date, data: Data, attributes: [String: String]) {
+        self.id = id
+        self.published = published
+        self.data = data
+        self.attributes = attributes
+    }
+
     public init(data: Data, attributes: [String: String] = [:]) {
         self.id = ""
         self.published = Date(timeIntervalSince1970: 0)
@@ -18,5 +25,9 @@ public struct Message {
     public init<Element: Encodable>(dataEncoding element: Element, attributes: [String: String] = [:]) throws {
         let data = try JSONEncoder().encode(element)
         self.init(data: data, attributes: attributes)
+    }
+
+    public func decode<Element: Decodable>(_ elementType: Element.Type) throws -> Element {
+        try JSONDecoder().decode(elementType, from: data)
     }
 }
