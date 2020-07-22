f819936050a031ba9e9cdaffe2043de623fcf88c
Gustaf Rosenblad <gustaf@ccode.se>
2020-07-22 18:34:50 +0200

    feat: Added support for emulator for publishers

--- Sources/GoogleCloudPubSub/PublisherDriver.swift
+++ Sources/GoogleCloudPubSub/PublisherDriver.swift
@@ -13,15 +13,30 @@ public final class PublisherDriver: Driver {
     public required init(eventLoopGroupProvider: EventLoopGroupProvider) throws {
         try super.init(eventLoopGroupProvider: eventLoopGroupProvider)
 
-        let channel = ClientConnection
-            .secure(group: eventLoopGroup)
-            .connect(host: "pubsub.googleapis.com", port: 443)
+        // Emulator
+        if let host = ProcessInfo.processInfo.environment["PUBSUB_EMULATOR_HOST"] {
+            let components = host.components(separatedBy: ":")
+            let port = Int(components[1])!
 
-        let callOptions = CallOptions(
-            customMetadata: ["authorization": "Bearer \(try accessToken())"]
-        )
+            let channel = ClientConnection
+                .insecure(group: eventLoopGroup)
+                .connect(host: components[0], port: port)
 
-        self.rawClient = Google_Pubsub_V1_PublisherClient(channel: channel, defaultCallOptions: callOptions)
+            self.rawClient = Google_Pubsub_V1_PublisherClient(channel: channel)
+        }
+
+        // Production
+        else {
+            let channel = ClientConnection
+                .secure(group: eventLoopGroup)
+                .connect(host: "pubsub.googleapis.com", port: 443)
+
+            let callOptions = CallOptions(
+                customMetadata: ["authorization": "Bearer \(try accessToken())"]
+            )
+
+            self.rawClient = Google_Pubsub_V1_PublisherClient(channel: channel, defaultCallOptions: callOptions)
+        }
 
         Self.`default` = self
     }
